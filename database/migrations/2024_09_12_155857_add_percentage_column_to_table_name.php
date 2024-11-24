584fe7eff8f4d6b545e5c9df639c83e37926680b
Adam LaCombe <git@adamlacombe.com>
2024-11-24 10:44:05 +0000

    Fixes error with table name casing in migrations
    Updates table name 'Notifications' to 'notifications' to match the actual table name in the database. This change corrects a SQLSTATE[42S02] error where the base table or view was not found due to incorrect casing.

--- database/migrations/2024_09_12_155857_add_percentage_column_to_table_name.php
+++ database/migrations/2024_09_12_155857_add_percentage_column_to_table_name.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Notifications', function (Blueprint $table) {
+        Schema::table('notifications', function (Blueprint $table) {
             $table->decimal('percentage', )->nullable(); 
         });
 
@@ -21,7 +21,7 @@ return new class extends Migration
      * Reverse the migrations.
      */
     public function down(): void
-    {Schema::table('Notifications', function (Blueprint $table) {
+    {Schema::table('notifications', function (Blueprint $table) {
         $table->dropColumn('percentage');
     });
     }
